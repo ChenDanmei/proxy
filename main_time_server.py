b9b80b91c88b8b75827b7aae35a3a4cf33744b22
ChenDanmei <chendmpolytech@gmail.com>
2017-06-23 17:11:30 +0200

    Create main_time_server.py

---
+++ main_time_server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import zmq
+from TimeServer import Server
+import pickle
+
+
+def time_server():
+
+    server = Server()
+    context = zmq.Context()
+    answer_time = context.socket(zmq.REP)
+    answer_time.bind("tcp://*:5558")
+    answer_time.recv()
+    time = server.get_time()
+    t = pickle.dumps(time)
+    answer_time.send(t)
+    print("send time {}".format(time))
+
+
+if __name__ == '__main__':
+    time_server()
