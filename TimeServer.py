0ec6b56a3a78f55ed138d41d84b92d3faa4c12a7
ChenDanmei <chendmpolytech@gmail.com>
2017-06-23 17:09:59 +0200

    Create TimeServer.py

---
+++ TimeServer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime as dt
+
+
+class Server():
+
+    def __init__(self):
+        pass
+
+    def get_time(self):
+
+        return dt.datetime.today()
