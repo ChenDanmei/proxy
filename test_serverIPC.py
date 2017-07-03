6669f46aa022107696d74a0eaae38c3375b48b4f
ChenDanmei <chendmpolytech@gmail.com>
2017-07-03 13:02:54 +0200

    Update test_serverIPC.py

--- test_serverIPC.py
+++ test_serverIPC.py
@@ -3,23 +3,28 @@
 
 import unittest
 from TimeServerProxyIPC import TimeServerProxyIPC
-from main_time_server import *
+from TimeServerIPC import TimeServerIPC
 import datetime as dt
+import time
 
 
-def time_server_process():
-    thread_server = TimeServerIPC()
-    thread_server.start()
-
-
-class TestStringMethods(unittest.TestCase):
+class TestIPCServer(unittest.TestCase):
 
     def test_IPC(self):
-        client = TimeServerProxyIPC()
-        time_server_process()
-        result=client.get_time() - dt.datetime.today()
-        #result= abs(dt.datetime.today()-client.get_time() )   # plus de 5ms
-        self.assertEqual(result.microseconds < 5, True)
+        socket_mode = 'zmq'
+        client = TimeServerProxyIPC(socket_mode= socket_mode)
+        server = TimeServerIPC(socket_mode=socket_mode)
+        server.start()
+        srv_time = client.get_time() 
+        server.stop()
+        while not server.stopped():
+            time.sleep(0.1)
+        print("try to join")
+        server.join()
+        print("joined")
+        result= abs(dt.datetime.today()-srv_time)   # plus de 5ms
+        print ("duration: "+str(result))
+        self.assertEqual(result.microseconds < 200*1000, True)
 
 if __name__ == '__main__':
     unittest.main()
